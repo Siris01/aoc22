e6d957b92d77cf41f69b615bd24d9c2089a0240d
Siris01 <40269790+Siris01@users.noreply.github.com>
2022-12-02 11:55:59 +0530

    perf: d2

--- src/d2/p2.rs
+++ src/d2/p2.rs
@@ -1,42 +1,42 @@
-pub fn run(input: &str) -> i64 {
+pub unsafe fn run(input: &str) -> i64 {
     let mut ans: i64 = 0;
     let lines = input.lines();
 
-    for l in lines {
-        let mut itr = l.split_whitespace();
+    unsafe {
+        for l in lines {
+            let a = match l.get_unchecked(0..1) {
+                "A" => 1,
+                "B" => 2,
+                _ => 3,
+            };
+            let res = match l.get_unchecked(2..3) {
+                "X" => 0,
+                "Y" => 3,
+                _ => 6,
+            };
 
-        let a = match itr.next().unwrap() {
-            "A" => 1,
-            "B" => 2,
-            _ => 3,
-        };
-        let res = match itr.next().unwrap() {
-            "X" => 0,
-            "Y" => 3,
-            _ => 6,
-        };
-
-        let b = match res {
-            0 => {
-                let r = a - 1;
-                if r == 0 {
-                    3
-                } else {
-                    r
+            let b = match res {
+                0 => {
+                    let r = a - 1;
+                    if r == 0 {
+                        3
+                    } else {
+                        r
+                    }
                 }
-            }
-            6 => {
-                let r = a + 1;
-                if r == 4 {
-                    1
-                } else {
-                    r
+                6 => {
+                    let r = a + 1;
+                    if r == 4 {
+                        1
+                    } else {
+                        r
+                    }
                 }
-            }
-            _ => a,
-        };
+                _ => a,
+            };
 
-        ans += b + res;
+            ans += b + res;
+        }
     }
 
     ans
