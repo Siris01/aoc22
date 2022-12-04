c847e0bb1c25c81b638c9723df7211987b89b628
Siris01 <40269790+Siris01@users.noreply.github.com>
2022-12-04 11:29:24 +0530

    perf(d4): p2

--- d4/p2.cs
+++ d4/p2.cs
@@ -4,7 +4,7 @@ class Program
 {
     public static void Main(string[] args)
     {
-        Int32 ans = 0;
+        Int32 x = 0, lines=0;
 
         foreach (string line in System.IO.File.ReadLines(@"input.txt"))
         {
@@ -12,13 +12,13 @@ class Program
             int[] a = Array.ConvertAll(jobs[0].Split('-'), s => int.Parse(s));
             int[] b = Array.ConvertAll(jobs[1].Split('-'), s => int.Parse(s));
 
-            if (a[0] == b[0] || a[0] == b[1] || b[1] == a[1] || b[0] == a[1]) ans++;
-            else if (a[0] >= b[0] && a[0] <= b[1]) ans++;
-            else if (a[1] >= b[0] && a[1] <= b[1]) ans++;
-            else if (b[0] >= a[0] && b[0] <= a[1]) ans++;
-            else if (b[1] >= a[0] && b[1] <= a[1]) ans++;
+            lines++;
+
+            if (a[1] < b[0]) x++;
+            else if (b[1] < a[0]) x++;
         }
 
+        Int32 ans = lines - x;
         Console.WriteLine(ans);
     }
 }
