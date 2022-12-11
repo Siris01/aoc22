1edcfef2e83cb2e4cba0eb5ca5111a6b462b10ae
Siris01 <40269790+Siris01@users.noreply.github.com>
2022-12-11 18:11:08 +0530

    day 11: part 2

--- d11/p2.hs
+++ d11/p2.hs
@@ -34,30 +34,30 @@ splitStr sub str = split' sub str [] []
         | sub `isPrefixOf` str = split' sub (drop (length sub) str) [] (reverse subacc:acc)
         | otherwise            = split' sub (tail str) (head str:subacc) acc
 
-processRound :: String -> [[Int]] -> [Int] -> Int -> Int
-processRound _ _ counts 20  =  product (take 2 (reverse (sort counts)))
-processRound str levels counts i = do
-    let (l, c) = processMonkeys (splitStr "\n\n" str) levels counts
-    processRound str l c (i+1)
+processRound :: String -> [[Int]] -> [Int] -> Int -> Int -> Int
+processRound _ _ counts 10000 _  =  product (take 2 (reverse (sort counts)))
+processRound str levels counts i m = do
+    let (l, c) = processMonkeys (splitStr "\n\n" str) levels counts m
+    processRound str l c (i+1) m
 
-processMonkeys :: [String] -> [[Int]] -> [Int] -> ([[Int]], [Int])
-processMonkeys [] levels counts = (levels, counts) 
-processMonkeys (x:xs) levels counts = do
+processMonkeys :: [String] -> [[Int]] -> [Int] -> Int -> ([[Int]], [Int])
+processMonkeys [] levels counts _ = (levels, counts) 
+processMonkeys (x:xs) levels counts m = do
     let (monkeyNumber, op, d, t, f) = parseMonkey (lines x)
-    let (l, c) = inspect (levels !! monkeyNumber) levels counts op d t f monkeyNumber
-    processMonkeys xs l c
+    let (l, c) = inspect (levels !! monkeyNumber) levels counts op d t f monkeyNumber m
+    processMonkeys xs l c m
 
-inspect :: [Int] -> [[Int]] -> [Int] -> (Int -> Int) -> Int -> Int -> Int -> Int -> ([[Int]], [Int])
-inspect [] levels counts _ _ _ _ i = (modify2dArray levels i [], counts)
-inspect (x:xs) levels counts op d t f monkeyNumber = do
-    let worry = op x `div` 3
+inspect :: [Int] -> [[Int]] -> [Int] -> (Int -> Int) -> Int -> Int -> Int -> Int -> Int -> ([[Int]], [Int])
+inspect [] levels counts _ _ _ _ i _ = (modify2dArray levels i [], counts)
+inspect (x:xs) levels counts op d t f monkeyNumber m = do
+    let worry = op x `mod` m
     let cnts = modifyArray counts monkeyNumber ((counts !! monkeyNumber) + 1)
     if worry `mod` d == 0 then do
         let stack = (levels !! t) ++ [worry]
-        inspect xs (modify2dArray levels t stack) cnts op d t f monkeyNumber
+        inspect xs (modify2dArray levels t stack) cnts op d t f monkeyNumber m
     else do
         let stack = (levels !! f) ++ [worry]
-        inspect xs (modify2dArray levels f stack) cnts op d t f monkeyNumber
+        inspect xs (modify2dArray levels f stack) cnts op d t f monkeyNumber m
         
 parseMonkey :: [String] -> (Int, Int -> Int, Int, Int, Int)
 parseMonkey ls = do
@@ -75,8 +75,13 @@ initLevels (x:xs) levels i = do
     let stack = map toInt (splitStr ", " (splitStr ": " x !! 1))
     initLevels xs (modify2dArray levels i stack) (i+1)
 
+initMod :: [String] -> Int -> Int
+initMod [] m = m
+initMod (x:xs) m = initMod xs (m * toInt (splitStr "by " x !! 1))
+
 main :: IO ()
 main = do
     file <- readFile "input.txt"
     let levels = initLevels (filter ("Starting items" `isInfixOf`) (splitStr "\n" file)) (replicate 8 []) 0
-    print (processRound file levels (replicate 8 0) 0)
+    let m = initMod (filter ("divisible" `isInfixOf`) (splitStr "\n" file)) 1
+    print (processRound file levels (replicate 8 0) 0 m)
